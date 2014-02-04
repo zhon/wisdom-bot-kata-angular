8ed9550828a5adfde5b62c13cde17745f943cabd
zhon <zhon@xputah.org>
2014-02-04 12:54:18 -0700

    Added more to about page

--- src/app/about/about.js
+++ src/app/about/about.js
@@ -15,12 +15,7 @@ angular.module( 'app.about', [
   });
 })
 
-.controller( 'AboutCtrl', function ( $scope ) {
-  $scope.dropdownDemoItems = [
-    "The first choice!",
-    "And another choice for you.",
-    "but wait! A third!"
-  ];
+.controller( 'AboutCtrl', function ( ) {
 })
 
 ;
